8a32164b2e3e27bca0bd3fa637742f66307ad98b
Thavi <thavit97@gmail.com>
2020-04-12 15:36:28 +0100

    can now add more select options
    Next: make sure each select box has a unique name and id

--- resources/views/graph.blade.php
+++ resources/views/graph.blade.php
@@ -3,14 +3,7 @@
 <script>
 $(document).ready(function(){
   $("#btn1").click(function(){
-    $("#country_select").append("<select id='countries' name='countries' class='form-control'>
-      @foreach($confirmed_cases_timeseries['data'] as $confirmed_case)
-        @if($confirmed_case['Province/State'] == '')
-          @php $confirmed_case_with_country = $confirmed_case['Country/Region']; @endphp
-          <option value='{{$confirmed_case_with_country}}'>{{$confirmed_case_with_country}}</option>
-        @endif
-      @endforeach
-    </select>");
+    $("#country_select").append("<select id='countries' name='countries' class='form-control'>@foreach($confirmed_cases_timeseries['data'] as $confirmed_case) @if($confirmed_case['Province/State'] == '')<option value='{{$confirmed_case[str_replace("'", "\\'", 'Country/Region')]}}'>{{$confirmed_case[str_replace("'", "\\'", 'Country/Region')]}}</option>@endif @endforeach</select>");
   });
 });
 </script>
@@ -26,7 +19,7 @@ $(document).ready(function(){
       <select id="countries" name="countries" class="form-control">
         @foreach($confirmed_cases_timeseries['data'] as $confirmed_case)
           @if($confirmed_case['Province/State'] == "")
-            <option value="{{$confirmed_case['Country/Region']}}">{{$confirmed_case['Country/Region']}}</option>
+            <option value="{{$confirmed_case[str_replace("'", "\\'", 'Country/Region')]}}">{{$confirmed_case[str_replace("'", "\\'", 'Country/Region')]}}</option>
           @endif
         @endforeach
       </select>
@@ -38,3 +31,11 @@ $(document).ready(function(){
 <script src=https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.2/echarts-en.min.js charset=utf-8></script>
 {!! $confirmed_chart->script() !!}
 @endsection
+
+<!--<select class='form-control'>
+@foreach($confirmed_cases_timeseries['data'] as $confirmed_case)
+  @if($confirmed_case['Province/State'] == '')
+    <option value='{{$confirmed_case[str_replace("'", "\\'", 'Country/Region')]}}'>{{$confirmed_case['Country/Region']}}</option>
+  @endif
+@endforeach
+</select>-->
